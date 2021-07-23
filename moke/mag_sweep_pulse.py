1a8f27c2b99509ff44257606148fa10cc6fda2ee
Akiyo Iwashina <akiyo@keio.jp>
2021-07-23 20:22:30 +0900

    feat: add function for setting timing

--- moke/mag_sweep_pulse.py
+++ moke/mag_sweep_pulse.py
@@ -2,19 +2,38 @@
 import visa
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+usleep = lambda x: time.sleep(x/1000000.0)
 
 # -----------------------------------------------------------
 # Function
 # -----------------------------------------------------------
-# def get_nearest_value_index(list, num):
-#     idx = np.abs(np.asarray(list) - num).argmin()
-#     return idx
+def set_pulse_timing(hold_time, meas_delay, period, pulse_width):
+    """
+    set pulse timing for ADCMT
+
+    Parameters
+    ----------
+    hold_time (ms)  : Th
+    meas_delay (ms) : Td
+    period (ms)     : Tp
+    pulse_width (ms): Tw
+
+    Constants for timing
+    -----
+    Tds(source delay time) + 300us < Tp
+    Td + 300us < Tp
+    Tds <= Td
+    Tds + Tw + 300us < Tp
+    600ms <= Tp
+    """
+    ADCMT.write('SP'+str(hold_time)+','+str(meas_delay)+','+str(period)+','+str(pulse_width))
 # -----------------------------------------------------------
 # Initialization
 # -----------------------------------------------------------
 rm = visa.ResourceManager()
 rm.list_resources()
-ADCMT = rm.open_resource('GPIB0::1::INSTR') # ADC Corp.,6240A,154080243,D00
+ADCMT = rm.open_resource('GPIB0::27::INSTR') # out-of-plane
 print(ADCMT.query('*IDN?'))
 
 ADCMT.write('*RST;*CLS')
@@ -27,16 +46,29 @@ ADCMT.write('IF')  # current output
 ADCMT.write('F1')  # voltage measurement
 ADCMT.write('MD1')  # pulse gen
 # -----------------------------------------------------------
+# Constants
+# -----------------------------------------------------------
+mag_pulse_curr = 0.1
+HOLD_TIME = 3
+MEAS_DELAY = 1
+PERIOD = 1300
+PULSE_WIDTH = 500
+# -----------------------------------------------------------
 # Trigger Settings:
 # -----------------------------------------------------------
-ADCMT.write('SOI0.003,LMI3')  # pulse 3mA, limit 3V
+ADCMT.write('LMV3')  # limit 3V
 ADCMT.write('DBI0')  # pulse base 0A
-ADCMT.write('SP3,1,130,50')  # hold time: 3ms, masure delay: 1ms, period: 130ms, pulse time 50ms
-ADCMT.write('OPR') # output on
+set_pulse_timing(HOLD_TIME, MEAS_DELAY, PERIOD, PULSE_WIDTH)
+for loop in range(100):
+    ADCMT.write('SOI%f' % 0.2)
+    ADCMT.write('OPR') # output on
+    usleep(100000)
+    ADCMT.write('SOI%f' % -0.2)
+    ADCMT.write('OPR') # output on
+    usleep(100000)
 ADCMT.timeout = 2000  # Acquisition timeout in milliseconds - set it higher than the acquisition time
 print('Waiting for the acquisition to finish... ')
 ADCMT.query('*OPC?')  # Using *OPC? query waits until the instrument finished the acquisition
-
 
 ADCMT.write('SBY')  # output off
 ADCMT.query('*OPC?')
